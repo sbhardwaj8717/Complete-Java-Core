27cb4c3e9b6d5f85b0102352dec98d9dbdf09fca
sbhardwaj8717 <sbhardwaj8717@gmail.com>
2020-09-22 16:28:12 +0530

    opreators

---
+++ 2. opreators/Airthmatic.java
@@ -0,0 +1,21 @@
+// program for airthmatic op
+
+
+import java.util.Scanner;
+public class Airthmatic{
+	public static void main(String... kuch){
+		Scanner num= new Scanner(System.in);
+		System.out.print("num1=");
+		int a=num.nextInt();
+		System.out.print("num2=");
+		int b=num.nextInt();
+		System.out.println("a + b ="+(a+b));
+		System.out.println("a - b ="+(a-b));
+		System.out.println("a * b ="+(a*b));
+		System.out.println("a / b ="+(a/b));
+		System.out.println("a % b ="+(a%b));
+	
+	}
+	
+
+}
