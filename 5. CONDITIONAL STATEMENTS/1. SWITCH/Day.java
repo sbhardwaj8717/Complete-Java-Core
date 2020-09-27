6e64e190ac763672640bf512c9b53ca49ad0a530
sbhardwaj8717 <sbhardwaj8717@gmail.com>
2020-09-27 21:17:29 +0530

    Switch Case

---
+++ 5. CONDITIONAL STATEMENTS/1. SWITCH/Day.java
@@ -0,0 +1,58 @@
+// program for days
+
+import java.util.Scanner;
+public class Day{
+	public static void main(String[] spapaji){
+		Scanner s8717=new Scanner(System.in);
+		System.out.print("ENTER A DAY NUMBER=");
+		int day=s8717.nextInt();
+		switch(day){
+			case 1: 
+				System.out.println(day +" is monday");
+				break;
+			case 2: 
+				System.out.println(day +" is tuesday");
+				break;
+			case 3: 
+				System.out.println(day +" is wednesday");	
+				break;
+			case 4: 
+				System.out.println(day +" is thrusday");
+				break;
+			case 5: 
+				System.out.println(day +" is friday");
+				break;
+			case 6: 
+				System.out.println(day +" is saturday");
+				break;
+			case 7: 
+				System.out.println(day +" is sunday");
+				break;
+			default:
+				System.out.println(day +" is not a day. You enter  a wrong choice");
+		}
+
+	}
+}
+
+
+			char inp = sc.nextChar();
+
+			switch(inp){
+				case 'a' :
+				
+
+				case 'e' : 
+				
+
+				case 'i' :
+				
+
+				case 'o' : 
+				
+
+				case 'u' : SYSOUT(vowel)
+				break;
+
+				default : sysout(consonent)
+			}
